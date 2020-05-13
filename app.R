b963ce58790a7661ae4b152a40402b005810ae14
kachergis <george.kachergis@gmail.com>
2020-05-13 15:52:03 -0700

    now using truncated normal distributions

--- app.R
+++ app.R
@@ -4,7 +4,22 @@
 # - add simulation with frequency distribution of our distance from neighboring stars
 
 
+# http://www.atlasoftheuniverse.com/50lys.html
+# 133 stars similar to the Sun within 50 light-years; probably many Earth-like planets around these stars. 
+# There are roughly 1400 star systems within this volume of space containing 2000 stars.
+
+# 33 stars within 12.5 light years
+# 250,000 stars within 250 light years
+
+
+# stars within X light-years: 
+# estimated 0.120 stars/cubic parsec number, and using a volume for a distance 
+# 100 light-years = 100/3.26 = 30.7 parsecs
+# Number = density * volume = 0.120 stars/cubic parsec  * 4/3 pi (30.7 parsecs)^3 = 14,600 stars
+# http://teacherlink.ed.usu.edu/tlnasa/reference/imaginedvd/files/imagine/docs/ask_astro/answers/980123d.html
+
 library(shiny)
+library(truncnorm)
 
 drake_eq <- function(R, fp, ne, fl, fi, fc, L) {
     expected_num_civs = R*fp*ne*fl*fi*fc*L
@@ -26,7 +41,7 @@ ui <- fluidPage(
         sidebarPanel(
             #mathjax.tags$div(HTML("")),
             # should include number of stars currently in the galaxy...
-            sliderInput("R", "$R =$ number of stars born in the Milky Way each year", min=0, max=100, value=2),
+            sliderInput("R", "$R =$ number of stars born in the Milky Way each year", min=0, max=25, value=10),
             helpText("Currently there are 100 billion stars in the Milky Way, with an estimated star formation rate of 1.5-3 stars per year."),
             sliderInput("fp", "$f_p =$ Proportion of stars that have planetary systems", min=0, max=1, value=1, step=.01),
             helpText("Recent estimates put $f_p$ close to 1, i.e. all stars have a planetary system."),
@@ -69,14 +84,15 @@ server <- function(input, output) {
     })
     
     get_drake_distro = reactive({
+        minp = 1e-7 # none of the probabilities / Ns can be 0, since Earth/humans exist..
         x = 10000 # number of worlds to simulate
-        Rd = rnorm(x, mean=input$R, sd=.1)
-        fpd = rnorm(x, mean=input$fp, sd=.1) # fraction of stars with planetary systems
-        ned = rnorm(x, mean=input$ne, sd=.5) # number of planets suitable for life per solar system
-        fld = rnorm(x, mean=input$fl, sd=.1) # fraction of suitable planets on which life actually appears.
-        fid = rnorm(x, mean=input$fi, sd=.1) # fraction of life-bearing planets on which intelligent life emerges
-        fcd = rnorm(x, mean=input$fc, sd=.1) # fraction of civilizations that develop and use a detectable communication technology
-        Ld = rnorm(x, mean=input$L, sd=10) # years such civilizations release detectable signals into space.
+        Rd = rtruncnorm(n=x, a=minp, b=Inf, mean=input$R, sd=1)
+        fpd = rtruncnorm(n=x, a=minp, b=1, mean=input$fp, sd=.1) # fraction of stars with planetary systems
+        ned = rtruncnorm(n=x, a=minp, b=Inf, mean=input$ne, sd=.5) # number of planets suitable for life per solar system
+        fld = rtruncnorm(n=x, a=minp, b=1, mean=input$fl, sd=.1) # fraction of suitable planets on which life actually appears.
+        fid = rtruncnorm(n=x, a=minp, b=1, mean=input$fi, sd=.1) # fraction of life-bearing planets on which intelligent life emerges
+        fcd = rtruncnorm(n=x, a=minp, b=1, mean=input$fc, sd=.1) # fraction of civilizations that develop and use a detectable communication technology
+        Ld = rtruncnorm(n=x, a=50, b=Inf, mean=input$L, sd=50) # years such civilizations release detectable signals into space.
         return(Rd * fpd * ned * fld * fid * fcd * Ld)
     })
     
@@ -84,17 +100,23 @@ server <- function(input, output) {
         civdist = get_drake_distro()
         civdist[which(civdist<0)] = 0 # can't have negative civilizations
         hist(civdist, col = 'darkgray', border = 'white', xlab="Number of Civilizations", 
-             main="Distribution of # of Broadcasting Civilizations in 10,000 Simulated Universes", 
+             main="Distribution of Broadcasting Civilizations in 10,000 Simulations", 
              freq=T) #  , xlim=c(0,1000)
         abline(v=median(civdist), lty="dotted")
-        text(x=(median(civdist)+3), y=5000, paste("Median:\n",round(median(civdist))))
+        #text(x=(median(civdist)+20), y=200, paste("Median:\n",round(median(civdist))))
+        abline(v=mean(civdist), lty="dashed", col="red")
+        #text(x=(mean(civdist)+40), y=400, paste("Mean:\n",round(mean(civdist))), col="red")
+        legend("topright",c(paste("Median =",round(median(civdist))),
+                            paste("Mean =",round(mean(civdist)))),
+               lty = c("dotted", "dashed"), col=c("black", "red"))
         print(mean(civdist))
         #ggplot() + geom_histogram(data = civdist, aes(x = )) + geom_hline(x=mean(civdist))
     })
     
     output$probMoreThanUs <- renderText({
         civdist = get_drake_distro()
-        paste("In this simulation, the probability that there is more than one communicating civilization in our galaxy is ", round(sum(civdist>1)/length(civdist),2), ".",sep='')
+        paste("In this simulation, the probability that there is more than one communicating civilization in our galaxy is ", 
+              round(sum(civdist>1)/length(civdist),2), ".",sep='')
     })
     
     output$printDrakeEqn <- renderText({ 
